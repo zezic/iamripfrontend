af6d75d782c57fb7885858962b735ffb6b3ac15c
damogingcidanrana <damogingcidanrana@mail.ru>
2016-05-14 16:50:25 +0300

    add git ignore and fix unclosed tags

--- js/register.js
+++ js/register.js
@@ -3,16 +3,16 @@ var Registration = React.createClass({
     return (
       <div class="form"><img class="emblem" src="images/emblem.svg">
       <div class="inputblock">
-        <input class="username" type="text">
+        <input class="username" type="text" />
       </div>
       <div class="inputblock">
-        <input class="email" type="text">
+        <input class="email" type="text" />
       </div>
       <div class="inputblock">
-        <input class="password" type="password">
+        <input class="password" type="password" />
       </div>
       <div class="inputblock">
-        <input class="passwordrepeat" type="password">
+        <input class="passwordrepeat" type="password" />
       </div>
       <button class="button">Register</button>
     </div>
