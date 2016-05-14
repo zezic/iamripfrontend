b18e866a0e477f95f403e872615376bd925fff08
Sergey Ukolov <zezic51@yandex.ru>
2016-05-15 02:19:42 +0300

    New JS build sys

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var pug = require('gulp-pug');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
+var addsrc = require('gulp-add-src'); 
 
 // Compile Our Sass
 gulp.task('sass', function() {
@@ -28,14 +30,25 @@ gulp.task('pug', function() {
 
 // Concat JS
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('js/vendor/*.js')
+        .pipe(uglify())
+        .pipe(addsrc('js/*.js'))
         .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('js/prod/'));
 });
+// Concat vendor JS
+//gulp.task('scripts-vendor', function() {
+    //return gulp.src('js/vendor/*.js')
+        //.pipe(concat('vendor-bundle.js'))
+        //.pipe(uglify())
+        //.pipe(gulp.dest('js/prod/'));
+//});
 
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('js/vendor/*.js', ['scripts']);
+    //gulp.watch('js/vendor/*.js', ['scripts-vendor']);
     gulp.watch('src/sass/*.sass', ['sass']);
     gulp.watch('src/sass/includes/*.sass', ['sass']);
     gulp.watch('src/pug/*.jade', ['pug']);
