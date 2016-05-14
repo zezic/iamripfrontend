1361933e1f4a5e5cd373288528c5fa13d1090134
damogingcidanrana <damogingcidanrana@mail.ru>
2016-05-14 19:58:27 -0400

    Fix JS CDN

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var pug = require('gulp-pug');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglifyjs');
 var addsrc = require('gulp-add-src'); 
+var order = require("gulp-order");
 
 // Compile Our Sass
 gulp.task('sass', function() {
@@ -31,23 +32,27 @@ gulp.task('pug', function() {
 // Concat JS
 gulp.task('scripts', function() {
     return gulp.src('js/vendor/*.js')
-        .pipe(uglify())
-        .pipe(addsrc('js/*.js'))
+	  .pipe(order([
+	    "js/vendor/react.js",
+	    "js/vendor/react-dom.js",
+	    "js/vendor/browser.js",
+	    "js/vendor/jquery.min.js",
+	    "js/vendor/marked.min.js"
+	  ]))
+        .pipe(concat('vendor-bundle.js'))
+        //.pipe(uglify())
+        .pipe(gulp.dest('js/prod/'));
+});
+gulp.task('scripts2', function() {
+    return gulp.src('js/*.js')
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('js/prod/'));
 });
-// Concat vendor JS
-//gulp.task('scripts-vendor', function() {
-    //return gulp.src('js/vendor/*.js')
-        //.pipe(concat('vendor-bundle.js'))
-        //.pipe(uglify())
-        //.pipe(gulp.dest('js/prod/'));
-//});
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('js/vendor/*.js', ['scripts']);
+    //gulp.watch('js/*.js', ['scripts2']);
+    //gulp.watch('js/vendor/*.js', ['scripts']);
     //gulp.watch('js/vendor/*.js', ['scripts-vendor']);
     gulp.watch('src/sass/*.sass', ['sass']);
     gulp.watch('src/sass/includes/*.sass', ['sass']);
@@ -56,5 +61,5 @@ gulp.task('watch', function() {
 });
 
 // Default Task
-gulp.task('default', ['sass', 'pug', 'scripts', 'watch']);
+gulp.task('default', ['sass', 'pug', 'watch']);
 // gulp.task('default', ['sass', 'watch']);
