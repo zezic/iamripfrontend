e6b6232ee5582830a889b71d18e39a150c661880
damogingcidanrana <damogingcidanrana@mail.ru>
2016-05-14 20:27:52 +0300

    register-modified but no testing

--- js/register.js
+++ js/register.js
@@ -1,21 +1,84 @@
 var Registration = React.createClass({
+  GetInitialState: function () {
+    return {
+      Username: '',
+      Email: '',
+      Password: ''
+    };
+  },
+  handleNameChange: function(e) {
+    this.setState({Username: e.target.value});
+  },
+  handleEmailChange: function(e) {
+    this.setState({Email: e.target.value});
+  },
+  handlePasswordChange: function(e) {
+    this.setState({Password: e.target.value});
+  },
+  RegistrationDataSubmit: function() {
+    dataForServer = JSON.stringify({username:Username, email:Email, password:Password});
+    this.setState({data:dataForServer});
+    $.ajax({
+      url: '/api/register',
+      dataType: 'json',
+      type: 'POST',
+      data:dataForServer,
+      contentType:'application/json',
+      success: function(data) {
+        this.setState({data: data});
+        GetInitialState();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({data:dataForServer});
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
-      <div className="form"><img className="emblem" src="images/emblem.svg"/>
+      <form 
+        className="form" 
+        onSubmit={this.RegistrationDataSubmit}
+      >
+      <img className="emblem" src="images/emblem.svg"/>
       <div className="inputblock">
-        <input className="username" type="text" />
+        <input 
+          className="username"
+          type="text" 
+          placeholder="Username"
+          value={this.state.Username}
+          onChange={this.handleNameChange}
+        />
       </div>
       <div className="inputblock">
-        <input className="email" type="text" />
+        <input 
+          className="email" 
+          type="text" 
+          placeholder="E-mail"
+          value={this.state.Email}
+          onChange={this.handleEmailChange}
+        />
       </div>
       <div className="inputblock">
-        <input className="password" type="password" />
+        <input 
+          className="password" 
+          type="password" 
+          placeholder="Password"
+          value={this.state.Password}
+          onChange={this.handlePasswordChange}
+        />
       </div>
       <div className="inputblock">
-        <input className="passwordrepeat" type="password" />
+        <input 
+          className="passwordrepeat" 
+          type="password" 
+          placeholder="Password"
+          value={this.state.Password}
+          onChange={this.handlePasswordChange}
+        />
       </div>
-      <button className="button">Register</button>
-    </div>
+      <button type="submit" className="button" onSubmit={this.RegistrationDataSubmit}>Register</button>
+    </form>
     );
   }
 });
